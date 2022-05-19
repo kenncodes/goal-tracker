e7ef48959a2943ea627082593aaaae991cb2f205
kj35 <olivewhitdev@gmail.com>
2022-05-18 23:55:48 -0400

    add progressbar and arrow to goal component

--- frontend/src/components/Goal.jsx
+++ frontend/src/components/Goal.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Badge from "react-bootstrap/Badge";
 import ProgressBar from "react-bootstrap/ProgressBar"
+
 import './Goal.css'
 
 const Goal = () => {
   return (
-    <article >
+    <article className="goalContainer" >
+      <div>
             <div>
               <Badge className="badge" bg="primary">
                 Medium
@@ -17,10 +19,13 @@ const Goal = () => {
                 Skill
               </Badge>
             </div>
+            <div className='arrowRow'> <h1> Learn to drive</h1> p</div>
+            <figcaption className="fig"> -- Learn to parallel park</figcaption>
             <div id="progressContainer">
             <ProgressBar striped animated variant="success" now={30} label="30%" />
             </div>
-           
+            </div>
+            <div></div>
           </article>
   )
 }
--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Goal from "../components/Goal";
 const Explore = () => {
   return (
-    <div className="goals">
+
       <Goal />
-    </div>
+   
   );
 };
 
